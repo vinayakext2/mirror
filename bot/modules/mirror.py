74563028f13876df545ebcfc84cbb310081a5d3b
Vinayak Mundakkal <76476634+vinayak-7-0-3@users.noreply.github.com>
2021-08-02 16:00:02 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -136,15 +136,15 @@ class MirrorListener(listeners.MirrorListeners):
 
     def onUploadComplete(self, link: str):
         with download_dict_lock:
-            msg = f'<b>Filename : </b><code>{download_dict[self.uid].name()}</code>\n\n<b>Size : </b><code>{download_dict[self.uid].size()}</code>'
+            msg = f'📁 <b>Filename</b> : <code>{download_dict[self.uid].name()}</code>\n\n<b>╭─ Size</b> : <code>{size}</code>'
             buttons = button_build.ButtonMaker()
-            buttons.buildbutton("☁️ Drive Link ☁️", link)
+            buttons.buildbutton("Drive Link", link)
             LOGGER.info(f'Done Uploading {download_dict[self.uid].name()}')
             if INDEX_URL is not None:
                 share_url = requests.utils.requote_uri(f'{INDEX_URL}/{download_dict[self.uid].name()}')
                 if os.path.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
                     share_url += '/'
-                buttons.buildbutton("⚡ Index Link ⚡", share_url)
+                buttons.buildbutton("Index Link", share_url)
             if BUTTON_THREE_NAME is not None and BUTTON_THREE_URL is not None:
                 buttons.buildbutton(f"{BUTTON_THREE_NAME}", f"{BUTTON_THREE_URL}")
             if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
@@ -156,7 +156,7 @@ class MirrorListener(listeners.MirrorListeners):
             else:
                 uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
             if uname is not None:
-                msg += f'\n\nHi {uname} \nYour File Has Been Mirrored 😊'
+                msg += f'\n<b>│\n├</b> {uname}\n<b>│ </b>Your File Has Been Mirrored\n<b>│\n╰───「 #mirrored 」</b>'
             try:
                 fs_utils.clean_download(download_dict[self.uid].path())
             except FileNotFoundError:
